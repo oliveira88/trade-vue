fa6e65a4c96c600390c2885a8091296d726558db
Rafa3lP <rafaelprenholato@hotmail.com>
2023-12-11 03:09:13 -0300

    currency input

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -1,55 +1,32 @@
-<!-- <template>
-  <input-mask
-    v-model="value"
-    :mask="currencyMask"
-    :always-show-mask="true"
-    :label="label"
-    :hint="hint"
-    :rules="rules"
-  />
+<template>
+  <v-text-field v-money="config" v-model.lazy="value" :label="label"></v-text-field>
 </template>
 
 <script>
-import { VueMaskDirective } from 'v-mask';
+import {VMoney} from 'v-money'
 
 export default {
-  directives: {
-    mask: VueMaskDirective,
-  },
-  data() {
-    return {
-      value: null,
-    };
-  },
-  computed: {
-    currencyMask() {
-      return 'R$ 0.00';
-    },
-    rules() {
-      return [
-        (v) => !!v || 'Campo obrigatório',
-      ];
-    },
-  },
-  methods: {
-    updateValue() {
-      // Remove o símbolo de moeda e converte para número
-      const numericValue = Number(this.value.replace(/[^\d.-]/g, ''));
-
-      // Atualiza o valor
-      this.value = isNaN(numericValue) ? null : numericValue;
-      this.$emit('input', this.value);
-    },
-  },
   props: {
     label: {
       type: String,
       default: 'Valor',
     },
-    hint: {
-      type: String,
-      default: 'Informe o valor em Reais',
-    },
   },
-};
-</script> -->
+  data() {
+    return {
+      config: {
+        decimal: ',',
+        thousands: '.',
+        prefix: 'R$ ',
+        precision: 2,
+        masked: false
+      }
+    }
+  },
+  directives: {money: VMoney},
+}
+</script>
+
+<style>
+
+</style>
